f40c3c763ac1a441b1045ee9dd66721b20c306ed
hhh57463 <hhh57463@naver.com>
2018-10-17 23:37:06 +0900

    Object Change Color

--- Assets/2_Script/HBullet.cs
+++ Assets/2_Script/HBullet.cs
@@ -4,16 +4,22 @@ using UnityEngine;
 
 public class HBullet : MonoBehaviour
 {
+    HGun _GunSc = null;
 
     GameObject _WorldGams = null;
+
+    Material _BulletMat = null;
 
     float _fBulletSpeed = 0.0f;
 
     void Start()
     {
         _WorldGams = GameObject.Find("World");
+        _GunSc = GameObject.Find("Gun").GetComponent<HGun>();
+        _BulletMat = GetComponent<MeshRenderer>().material;
         _fBulletSpeed = 5.0f;
-        StartCoroutine(ChangeParent());
+        BulletColorMng();
+        transform.parent = _WorldGams.transform;
     }
 
     void Update()
@@ -21,10 +27,38 @@ public class HBullet : MonoBehaviour
         transform.Translate(Vector3.forward * _fBulletSpeed * Time.deltaTime);
     }
 
-    IEnumerator ChangeParent()
+    void BulletColorMng()                              //총알 색 변환 함수
     {
-        yield return new WaitForSeconds(0.01f);
-        transform.parent = _WorldGams.transform;
+        switch (_GunSc._nBulletType)                   //총알 색 추가 될 수 있으니 switch로 해놓음
+        {
+            case 1:
+                _BulletMat.color = Color.red;
+                break;
+            case 2:
+                _BulletMat.color = Color.green;
+                break;
+            case 3:
+                _BulletMat.color = Color.blue;
+                break;
+        }
+    }
+
+    private void OnCollisionEnter(Collision col)
+    {
+        if (col.transform.tag.Equals("Object"))
+        {
+            Debug.Log("충돌!");
+            if (_GunSc._nBulletType.Equals(1))
+                col.transform.GetComponent<MeshRenderer>().material.color = Color.red;
+
+            else if (_GunSc._nBulletType.Equals(2))
+                col.transform.GetComponent<MeshRenderer>().material.color = Color.green;
+
+            else if (_GunSc._nBulletType.Equals(3))
+                col.transform.GetComponent<MeshRenderer>().material.color = Color.blue;
+
+            Destroy(gameObject);
+        }
     }
 
 }
--- Assets/2_Script/HGun.cs
+++ Assets/2_Script/HGun.cs
@@ -9,12 +9,15 @@ public class HGun : MonoBehaviour
 
     float _fDelay = 0.0f;
 
+    public int _nBulletType = 0;                               //총알의 색깔을 알아오는 변수 일단 빨강만 설정해놓음  1:빨강 2:초록 3:파랑
+
     bool _bBulletShot = false;
     bool _bBulletAccept = false;
 
     // Use this for initialization
     void Start()
     {
+        _nBulletType = 1;
         _fDelay = 1.0f;
     }
 
@@ -31,11 +34,29 @@ public class HGun : MonoBehaviour
         {
             if (!_bBulletAccept)
             {
-                Debug.Log("발사");
+                //Debug.Log("발사");
+                //_nBulletType = 1;                   //빨강
                 Instantiate(_BulletPrefab, transform.position, Quaternion.identity);
                 _bBulletAccept = true;
             }
         }
+        //////////////////////////////////////////////////나중에 지울 코드////////////////////////////////////////////////////////////
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            Debug.Log("Red");
+            _nBulletType = 1;
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            Debug.Log("Green");
+            _nBulletType = 2;
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            Debug.Log("Blue");
+            _nBulletType = 3;
+        }
+        //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
     }
 
     void DelayCount()
@@ -45,7 +66,7 @@ public class HGun : MonoBehaviour
             _fDelay = Time.time;
             _bBulletShot = true;
         }
-        if (Time.time > _fDelay + 3f)                                //연사속도
+        if (Time.time > _fDelay + 0.5f)                                //연사속도
         {
             _bBulletAccept = false;
             _bBulletShot = false;
