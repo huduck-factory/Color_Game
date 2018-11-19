181e853511ac750a00e341e896bc430a125f6b1d
hhh57463 <hhh57463@naver.com>
2018-11-19 19:41:32 +0900

    Bullet Reload System

--- Assets/2_Script/Player/HGun.cs
+++ Assets/2_Script/Player/HGun.cs
@@ -13,10 +13,11 @@ public class HGun : MonoBehaviour
 
     bool _bBulletShot = false;
     bool _bBulletAccept = false;
+    bool _bReload = false;
 
     void Start()
     {
-        if (SGameMng.I.mode.Equals(E_GAMEMODE.E_DEBUG)) { SGameMng.I.nBulletCount = 9999; }
+        if (SGameMng.I.mode.Equals(E_GAMEMODE.E_DEBUG)) { SGameMng.I.nBulletCount = 10; }
         else { SGameMng.I.nBulletCount = 10; }
         _nBulletType = (int)E_COLOR.E_RED;
         _fDelay = 1.0f;
@@ -25,24 +26,44 @@ public class HGun : MonoBehaviour
     void Update()
     {
         DelayCount();
-        if (Input.GetMouseButtonDown(0)){ BulletShot(); }                //마우스 클릭시 함수가 불러오는데 이 함수에서 총알 색바꾸는 코드가 있어서 마우스 누르는 동시에 번호를 눌러야함
+        BulletShot();            //마우스 클릭시 함수가 불러오는데 이 함수에서 총알 색바꾸는 코드가 있어서 마우스 누르는 동시에 번호를 눌러야함
     }
 
     void BulletShot()
     {
         if (SGameMng.I.nBulletCount > 0)
         {
-            if (!_bBulletAccept)
+            if (Input.GetMouseButtonDown(0))
             {
-                //_nBulletType = 1;         //빨강
-                SGameMng.I.nBulletCount--;
-                Instantiate(_BulletPrefab, transform.position, SGameMng.I.Htscrp.cam.transform.rotation);       // 카메라 방향으로 회전설정
-                _bBulletAccept = true;
+                if (!_bReload)
+                {
+                    if (!_bBulletAccept)
+                    {
+                        //_nBulletType = 1;         //빨강
+                        SGameMng.I.nBulletCount--;
+                        Instantiate(_BulletPrefab, transform.position, SGameMng.I.Htscrp.cam.transform.rotation);       // 카메라 방향으로 회전설정
+                        _bBulletAccept = true;
+                    }
+                }
             }
         }
-        else  { Debug.Log("총알 부족"); }
+        else
+        {
+            Debug.Log("총알 부족");
+            _bReload = true;
+            StartCoroutine(BulletReload());
+        }
 
-       // ---- 나중에 지울코드
+        if (SGameMng.I.nBulletCount < 10)
+        {
+            if (Input.GetKeyDown(KeyCode.R))
+            {
+                _bReload = true;
+                StartCoroutine(BulletReload());
+            }
+        }
+
+        // ---- 나중에 지울코드
         if (Input.GetKeyDown(KeyCode.Alpha1))
         {
             Debug.Log("Red");
@@ -74,4 +95,12 @@ public class HGun : MonoBehaviour
             _bBulletShot = false;
         }
     }
+
+    IEnumerator BulletReload()
+    {
+        yield return new WaitForSeconds(1.5f);
+        _bReload = false;
+        SGameMng.I.nBulletCount = 10;
+    }
+
 }
